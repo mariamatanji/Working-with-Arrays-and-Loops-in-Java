8f0d6c349fe0e33d9991102b7cf98d7dcbc71041
mariamatanji <38054680+mariamatanji@users.noreply.github.com>
2025-11-01 14:57:51 -0700

    Create ArrayAccess class to demonstrate array usage
    This Java exercise demonstrates how to create, initialize, and access elements of an integer array.
    It includes two main parts:
    
    Accessing array elements without using a loop — Each element is printed individually.
    
    Accessing array elements using a for loop — The program iterates through the array to print all elements efficiently.

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class ArrayAccess {
+    public static void main(String s[]) {
+        int arr[] = new int[10];
+        System.out.println(arr[0]+ "," +arr[1]+ "," +arr[2]+ "," +arr[3]+ "," +arr[4]+ "," +arr[5]+ "," +arr[6]+ "," +arr[7]+ "," +arr[8]+ "," +arr[9]);
+        int count_arr = arr.length;
+        for (int i=0; i<count_arr; i++) {
+            System.out.println(arr[i]);
+        }
+        }
+    }
